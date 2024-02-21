9a16c2e582624bbbe02e5cb04b924b6aa44b078c
Raquel Benito <raquelbenitop@gmail.com>
2024-02-21 15:19:12 +0100

    First Commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
  // Aquí tu código
+const btn = document.getElementById("agregar")
+let element = ""
+const lista = document.getElementById("lista")
+
+//Método 1
+btn.addEventListener("click", function(){
+    element= prompt("Escribe qué elemento quieres agregar")
+    lista.insertAdjacentHTML("beforeend", "<li>"+element+"</li><hr>")
+    console.log(element)
+})
+
+//Método 2
+/*
+btn.addEventListener("click", function(){
+    element= document.createTextNode(prompt("Escribe qué quieres agregar"))
+    let nuevoElemento = document.createElement("li")
+    nuevoElemento.appendChild(element)
+    lista.appendChild(nuevoElemento)
+})
+*/
